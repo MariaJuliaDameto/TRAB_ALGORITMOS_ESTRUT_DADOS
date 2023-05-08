b4c1ce55afb7b577950a943e5bee12b30bd42421
Maria Julia Dameto <107319655+MariaJuliaDameto@users.noreply.github.com>
2023-05-08 11:23:48 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from Desktop import Desktop
+from Notebook import Notebook
+from Computador import Computador
+
+
+desktop = Desktop("NEO 50s i5-12400/8GB/SSD256GB", 4500, "Preto", "500W")
+print(desktop.getConfiguracao())
+desktop.cadastrar()
+
+notebook = Notebook("Dell Latitude 3420 i7/16GB/SSD512M.2", 6000, "PRATA", "12 horas")
+print(notebook.getConfiguracao())
+notebook.cadastrar()
