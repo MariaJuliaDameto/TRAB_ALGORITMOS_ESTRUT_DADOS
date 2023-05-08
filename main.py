3ed91368208cd915486d24bdb2bc70e9f49cb153
Maria Julia Dameto <107319655+MariaJuliaDameto@users.noreply.github.com>
2023-05-08 19:30:39 -0300

    main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from Computador import Computador
 
 
 desktop = Desktop("NEO 50s i5-12400/8GB/SSD256GB", 4500, "Preto", "500W")
-print(desktop.getConfiguracao())
+print(desktop.getInformacoes())
 desktop.cadastrar()
 
 notebook = Notebook("Dell Latitude 3420 i7/16GB/SSD512M.2", 6000, "PRATA", "12 horas")
-print(notebook.getConfiguracao())
+print(notebook.getInformacoes())
 notebook.cadastrar()
