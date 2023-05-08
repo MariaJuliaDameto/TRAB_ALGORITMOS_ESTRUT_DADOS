9c536e36fa6b82371ab91e8fc150fffeae950c78
Maria Julia Dameto <107319655+MariaJuliaDameto@users.noreply.github.com>
2023-05-08 11:23:19 -0300

    Create Notebook.py

---
+++ Notebook.py
@@ -0,0 +1,14 @@
+from Computador import Computador
+
+class Notebook(Computador):
+    def _init_(self, modelo, preco, cor, duracaoBateria):
+        self.modelo = modelo
+        self.preco = preco
+        self.cor = cor
+        self.duracaoBateria = duracaoBateria
+
+    def getConfiguracao(self):
+        return f"Modelo: {self.modelo}, Cor: {self.cor}, Preço: {self.preco}, Tempo de Bateria: {self.duracaoBateria}"
+
+    def cadastrar(self):
+        print("As configurações do seu notebook foram salvas.")
